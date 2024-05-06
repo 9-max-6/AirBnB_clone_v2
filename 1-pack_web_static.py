9b35acc84b458814e5ace5f558d63868995462b3
9-max-6 <mutukumaxwel@gmail.com>
2024-05-06 19:56:19 +0300

    Fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,20 +4,19 @@ Fabric script to create archive
 """
 
 from datetime import datetime
-from fabric import *
+from fabric.api import *
 
 
+@task
 def do_pack():
     """
     making an archive best on the web_static folder
     """
-    with Connection(host='127.0.0.1') as c:
-        static_archive = 'web_static_' + datetime.now().strftime(
-            "%Y%m%d%H%M%S") + '.' + 'tgz'
-        c.local('mkdir -p versions')
-        create = c.local(f'tar -cvzf versions/{static_archive} web_static')
-        if create is not None:
-            return static_archive
-        else:
-            return None
-do_pack()
+    static_archive = 'web_static_' + datetime.now().strftime(
+        "%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local(f'tar -cvzf versions/{static_archive} web_static')
+    if create is not None:
+        return static_archive
+    else:
+        return None
