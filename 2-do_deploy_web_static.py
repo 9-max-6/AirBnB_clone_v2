86f89b19c79df8f9189595f4ff1f4755520696f0
9-max-6 <mutukumaxwel@gmail.com>
2024-05-08 14:16:55 +0300

    Feature: changed the file to do the ting.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,18 +10,22 @@ env.hosts = [
     '54.167.150.28'
     ]
 
+
 @task
 def do_deploy(archive_path):
     """return False if the file at the parth archive_path doesn't exist."""
     archive_name = archive_path.split('/')[-1]
-
+    new_archive = archive_name.split(".")[0]
     put(archive_path, f'/tmp/{archive_name}')
 
     with cd('/data/web_static/releases'):
-        run(f'mkdir -p {archive_name.split(".")[0]}')
-        run(f'tar -xzvf /tmp/{archive_name} -C {archive_name.split(".")[0]}')
+        run(f'mkdir -p {new_archive}')
+        run(f'tar -xzvf /tmp/{archive_name} -C {new_archive}')
     with cd('/tmp'):
         run(f'rm /tmp/{archive_name}')
     with cd('/data/web_static'):
         run('rm -rf current')
-        run(f'ln -s /data/web_static/releases/{archive_name.split(".")[0]} current')
+        command = 'ln -sf /data/web_static/releases/'
+        run(f'{command}{new_archive} current')
+    with cd(f'/data/web_static/releases/{new_archive}'):
+        run(f'mv * ..')
